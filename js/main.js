44551074e30021721bcc929cc05f55cdb02bba7e
KimByungHun <k01087831451@gmail.com>
2021-12-20 02:23:02 +0900

    button클릭시 배경 활성화 기능 추가

--- js/main.js
+++ js/main.js
@@ -29,3 +29,35 @@ const swiper = new Swiper("#wrap",{
     //     slideShadows:false
     // }
 });
+
+//DOM Cashing
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next"); 
+
+next.addEventListener("click",activation);
+prev.addEventListener("click",activation);
+window.addEventListener("mousewheel",activation);
+swiper.on("slideChangeTransitionEnd");
+const navi = document.querySelectorAll(".swiper-pagination span")
+
+for(let el of navi){
+    el.addEventListener("click",e=>{
+        const isOn = e.currentTarget.classList.contains(".swiper-pagination-bullet-active")
+        
+        swiper.on("slideChangeTransitionEnd",activation);
+        if(isOn) return;
+    })
+}
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+
+    for(let el of bgs)    {
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+
+}
